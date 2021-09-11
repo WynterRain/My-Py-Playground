06c8fa57638dbfd3ab47459e27678b346979ba1e
SnowiWatermelon <heonaliu@gmail.com>
2021-09-11 14:28:59 -0400

    Add more items to list in the randomly generated variables

--- Project_Password_Generator/Password_Generator.py
+++ Project_Password_Generator/Password_Generator.py
@@ -19,10 +19,28 @@ import random
 # 1+ Numberic Digit
 # 1+ Special Symbol( ! @ # $ % ^ & * ( ) + - = _ ~ )
 
-upper_character = ['A', 'B', 'C', 'D', 'E']
-lower_chracter = ['a','b','c','d','e']
-numbers = ['1', '2', '3', '4', '5']
-special_characters = ['@', '#', '^', '&', '!']
+upper_character = [
+                'A', 'B', 'C', 'D', 'E'
+                'F','G','H','I','J',
+                'K','L','M','N','O',
+                'P','Q','R','S','T',
+                'U','V','W','X','Y','Z'
+                    ]
+lower_chracter = [
+                'a','b','c','d','e'
+                'f','g','h','i','j',
+                'k','l','m','n','o',
+                'p','q','r','s','t',
+                'u','v','w','x','y','z'
+                  ]
+numbers = [
+        '1', '2', '3', '4', '5
+        '6','7','8','9'
+            ]
+special_characters = [
+                    '!','@','#','$',
+                    '%','^','&','*'
+                        ]
 
 password = (
             random.choice(upper_character) 
