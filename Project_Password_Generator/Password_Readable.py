3e83f285c7e2f7a57a72284091ad32d1aca8032a
SnowiWatermelon <heonaliu@gmail.com>
2021-09-11 15:08:45 -0400

    Open Wiki File; Print words with 5 + charactes Append those items to a list and randomize them .

--- Project_Password_Generator/Password_Readable.py
+++ Project_Password_Generator/Password_Readable.py
@@ -1,3 +1,18 @@
 import random
 
 word_list = []
+
+
+with open("Wiki_BT_Text.txt", 'r') as file:
+    data = file.readlines()
+    
+    for line in data:
+        words = line.split()
+    
+        for item in words:
+            if len(item) > 5:
+                word_list.append(item)
+                
+                
+rand_word = (random.choice(word_list))
+special_character = random
