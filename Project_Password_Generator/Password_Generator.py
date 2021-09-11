b1997f1936ea926d26674dfef939b6a28451c5e4
SnowiWatermelon <heonaliu@gmail.com>
2021-09-11 14:15:44 -0400

    Rename Variable password to password_p1

--- Project_Password_Generator/Password_Generator.py
+++ Project_Password_Generator/Password_Generator.py
@@ -24,7 +24,7 @@ lower_chracter = ['a','b','c','d','e']
 numbers = ['1', '2', '3', '4', '5']
 special_characters = ['@', '#', '^', '&', '!']
 
-password = (
+password_p1 = (
             random.choice(upper_character) 
             + random.choice(lower_chracter) 
             + random.choice(numbers) 
