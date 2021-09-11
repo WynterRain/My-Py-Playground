b770cddc9cbda7880f4063b5bbef3a20711059db
SnowiWatermelon <heonaliu@gmail.com>
2021-09-11 14:13:07 -0400

    Turn integers into string integers

--- Project_Password_Generator/Password_Generator.py
+++ Project_Password_Generator/Password_Generator.py
@@ -21,7 +21,7 @@ import random
 
 upper_character = ['A', 'B', 'C', 'D', 'E']
 lower_chracter = ['a','b','c','d','e']
-numbers = [1, 2, 3, 4, 5]
+numbers = ['1', '2', '3', '4', '5']
 special_characters = ['@', '#', '^', '&', '!']
 
 password = random.choice(upper_character) + random.choice(lower_chracter) + random.choice(numbers) + random.random.choice(special_characters)
