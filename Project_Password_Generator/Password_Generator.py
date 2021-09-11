84b4b864b3e3d8756832879f1d6604a578a2d58d
SnowiWatermelon <heonaliu@gmail.com>
2021-09-11 13:52:40 -0400

    Added comments / notes about random.randint

--- Project_Password_Generator/Password_Generator.py
+++ Project_Password_Generator/Password_Generator.py
@@ -3,3 +3,8 @@ import random
 # random.choice
     # picks a random choice out of a list / tuple / dictionary
     # e.g. print(random.choice(<list/tuple/dict name>))
+    
+# random.randint
+    # picks a random integer from a range of numbers
+    # e.g. print(random.randint(<min #>,<max #>)) 
+    
