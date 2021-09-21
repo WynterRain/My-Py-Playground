6bd142b71f8cf99bc49bad7c97dfd0bbf4bbcc0f
SnowiWatermelon <heonaliu@gmail.com>
2021-09-20 21:05:07 -0400

    Import requests and json module, use api to grab content

---
+++ Project_Get_Data_with_CoinMarketCap_API/Py_Crypto/Main.py
@@ -0,0 +1,8 @@
+import requests
+import json
+
+api_request = requests.get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=5&convert=USD&CMC_PRO_API_KEY=b2836bb2-a6a2-45e3-80a7-14a3810fe1e6")
+
+api = json.loads(api_request.content)
+
+print(api)
