daf9727c925ed64d3aa699a41bb2a0a99562ecc2
SnowiWatermelon <heonaliu@gmail.com>
2021-09-25 11:31:54 -0400

    Add name + symbol

--- Project_Get_Data_with_CoinMarketCap_API/Py_Crypto/Main.py
+++ Project_Get_Data_with_CoinMarketCap_API/Py_Crypto/Main.py
@@ -21,7 +21,7 @@ invested_coins = [
 for index in range(0,5):
     for coin in invested_coins:
         if api["data"][index]["symbol"] == coin["symbol"]:
-            print(api["data"][index]["symbol"])
+            print(api["data"][index]["name"] + " ✿ " + api["data"][index]["symbol"])
             print("{0:.2f}".format(api["data"][index]["quote"]["USD"]["price"]))
             # ("{0:.2f}".format -- reformats the output to be 2 decimals
             print("━━━━━━━☆☆━━━━━━━…‥・")
