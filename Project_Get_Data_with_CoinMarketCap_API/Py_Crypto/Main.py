156933c4e4228ffb31985dae1185047de655bf19
SnowiWatermelon <heonaliu@gmail.com>
2021-09-25 21:48:07 -0400

    Add variables / dividers

--- Project_Get_Data_with_CoinMarketCap_API/Py_Crypto/Main.py
+++ Project_Get_Data_with_CoinMarketCap_API/Py_Crypto/Main.py
@@ -5,6 +5,9 @@ api_request = requests.get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/
 
 api = json.loads(api_request.content)
 
+print("━━━━━━━☆☆━━━━━━━…‥・")
+print("━━━━━━━☆☆━━━━━━━…‥・")
+
 invested_coins = [
     {
       "symbol" : "BTC",
@@ -18,12 +21,28 @@ invested_coins = [
     }
 ]
 
+total_p_l = 0
+
 for index in range(0,5):
     for coin in invested_coins:
         if api["data"][index]["symbol"] == coin["symbol"]:
             total_paid = coin["amount_owned"] * coin["price_per_coin"]
+            current_value = coin["amount_owned"] * api["data"][index]["quote"]["USD"]["price"]
+            p_l_percoin = api["data"][index]["quote"]["USD"]["price"] - coin["price_per_coin"]
+            total_p_l_coin = p_l_percoin * coin["amount_owned"]
+            
+            total_p_l =+ total_p_l_coin
+            
             print(api["data"][index]["name"] + " ✿ " + api["data"][index]["symbol"])
             print("Price ✿ ${0:.2f}".format(api["data"][index]["quote"]["USD"]["price"]))
             # ("{0:.2f}".format -- reformats the output to be 2 decimals
             print("Total Amount Paid:", "${0:.2f}".format(total_paid))
+            print("Current Value:", "${0:.2f}".format(current_value))
+            print("P/L Per Coin:", "${0:.2f}".format(p_l_percoin))
+            print("Total P/L With Coin:", "${0:.2f}".format(total_p_l_coin))
             print("━━━━━━━☆☆━━━━━━━…‥・")
+            
+print("Total P/L For Portfolio:", "${0:.2f}".format(total_p_l))
+
+print("━━━━━━━☆☆━━━━━━━…‥・")
+print("━━━━━━━☆☆━━━━━━━…‥・")
