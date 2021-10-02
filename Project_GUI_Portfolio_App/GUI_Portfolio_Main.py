278bb1c2dc1e40ff726e4d20a9afcdba8fea02dc
SnowiWatermelon <heonaliu@gmail.com>
2021-10-02 14:47:12 -0400

    Modify Labes

--- Project_GUI_Portfolio_App/GUI_Portfolio_Main.py
+++ Project_GUI_Portfolio_App/GUI_Portfolio_Main.py
@@ -43,11 +43,33 @@ def my_portfolio():
                 print(api["data"][index]["name"] + " ✿ " + api["data"][index]["symbol"])
                 print("Price ✿ ${0:.2f}".format(api["data"][index]["quote"]["USD"]["price"]))
                 # ("{0:.2f}".format -- reformats the output to be 2 decimals
+                print("Number Of Coin:", coin["amount_owned"])
                 print("Total Amount Paid:", "${0:.2f}".format(total_paid))
                 print("Current Value:", "${0:.2f}".format(current_value))
                 print("P/L Per Coin:", "${0:.2f}".format(p_l_percoin))
                 print("Total P/L With Coin:", "${0:.2f}".format(total_p_l_coin))
                 print("━━━━━━━☆☆━━━━━━━…‥・")
+                
+                name = Label(pycrypto, text=api["data"][index]["symbol"], bg="grey", fg="black")
+                name.grid(row=0, column=0, sticky=N+S+E+W)
+
+                price = Label(pycrypto, text="${0:.2f}".format(api["data"][index]["quote"]["USD"]["price"]), bg="white", fg="black")
+                price.grid(row=0, column=1, sticky=N+S+E+W)
+
+                no_coins = Label(pycrypto, text=coin["amount_owned"], bg="grey", fg="black")
+                no_coins.grid(row=0, column=2, sticky=N+S+E+W)
+
+                total_paid = Label(pycrypto, text="${0:.2f}".format(total_paid), bg="white", fg="black")
+                total_paid.grid(row=0, column=3, sticky=N+S+E+W)
+
+                current_value = Label(pycrypto, text="${0:.2f}".format(current_value), bg="grey", fg="black")
+                current_value.grid(row=0, column=4, sticky=N+S+E+W)
+
+                p_l_coin = Label(pycrypto, text="${0:.2f}".format(p_l_percoin), bg="white", fg="black")
+                p_l_coin.grid(row=0, column=5, sticky=N+S+E+W)
+
+                total_p_l = Label(pycrypto, text="${0:.2f}".format(total_p_l_coin), bg="grey", fg="black")
+                total_p_l.grid(row=0, column=6, sticky=N+S+E+W)
 
     print("Total P/L For Portfolio:", "${0:.2f}".format(total_p_l))
 
