f3b78c9850b3ad21c18fd51c286751fb05b21985
SnowiWatermelon <heonaliu@gmail.com>
2021-10-02 12:04:06 -0400

    Uncomment

--- Project_GUI_Portfolio_App/GUI_Portfolio_Main.py
+++ Project_GUI_Portfolio_App/GUI_Portfolio_Main.py
@@ -17,48 +17,48 @@ pycrypto.mainloop()
 
 
 
-# api_request = requests.get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=5&convert=USD&CMC_PRO_API_KEY=b2836bb2-a6a2-45e3-80a7-14a3810fe1e6")
+api_request = requests.get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=5&convert=USD&CMC_PRO_API_KEY=b2836bb2-a6a2-45e3-80a7-14a3810fe1e6")
 
-# api = json.loads(api_request.content)
+api = json.loads(api_request.content)
 
-# print("━━━━━━━☆☆━━━━━━━…‥・")
-# print("━━━━━━━☆☆━━━━━━━…‥・")
+print("━━━━━━━☆☆━━━━━━━…‥・")
+print("━━━━━━━☆☆━━━━━━━…‥・")
 
-# invested_coins = [
-#     {
-#       "symbol" : "BTC",
-#       "amount_owned" : 2,
-#       "price_per_coin": 3200
-#     },
-#     {
-#       "symbol" : "BNB",
-#       "amount_owned" : 100,
-#       "price_per_coin": 2.05
-#     }
-# ]
+invested_coins = [
+    {
+      "symbol" : "BTC",
+      "amount_owned" : 2,
+      "price_per_coin": 3200
+    },
+    {
+      "symbol" : "BNB",
+      "amount_owned" : 100,
+      "price_per_coin": 2.05
+    }
+]
 
-# total_p_l = 0
+total_p_l = 0
 
-# for index in range(0,5):
-#     for coin in invested_coins:
-#         if api["data"][index]["symbol"] == coin["symbol"]:
-#             total_paid = coin["amount_owned"] * coin["price_per_coin"]
-#             current_value = coin["amount_owned"] * api["data"][index]["quote"]["USD"]["price"]
-#             p_l_percoin = api["data"][index]["quote"]["USD"]["price"] - coin["price_per_coin"]
-#             total_p_l_coin = p_l_percoin * coin["amount_owned"]
+for index in range(0,5):
+    for coin in invested_coins:
+        if api["data"][index]["symbol"] == coin["symbol"]:
+            total_paid = coin["amount_owned"] * coin["price_per_coin"]
+            current_value = coin["amount_owned"] * api["data"][index]["quote"]["USD"]["price"]
+            p_l_percoin = api["data"][index]["quote"]["USD"]["price"] - coin["price_per_coin"]
+            total_p_l_coin = p_l_percoin * coin["amount_owned"]
             
-#             total_p_l =+ total_p_l_coin
+            total_p_l =+ total_p_l_coin
             
-#             print(api["data"][index]["name"] + " ✿ " + api["data"][index]["symbol"])
-#             print("Price ✿ ${0:.2f}".format(api["data"][index]["quote"]["USD"]["price"]))
-#             # ("{0:.2f}".format -- reformats the output to be 2 decimals
-#             print("Total Amount Paid:", "${0:.2f}".format(total_paid))
-#             print("Current Value:", "${0:.2f}".format(current_value))
-#             print("P/L Per Coin:", "${0:.2f}".format(p_l_percoin))
-#             print("Total P/L With Coin:", "${0:.2f}".format(total_p_l_coin))
-#             print("━━━━━━━☆☆━━━━━━━…‥・")
+            print(api["data"][index]["name"] + " ✿ " + api["data"][index]["symbol"])
+            print("Price ✿ ${0:.2f}".format(api["data"][index]["quote"]["USD"]["price"]))
+            # ("{0:.2f}".format -- reformats the output to be 2 decimals
+            print("Total Amount Paid:", "${0:.2f}".format(total_paid))
+            print("Current Value:", "${0:.2f}".format(current_value))
+            print("P/L Per Coin:", "${0:.2f}".format(p_l_percoin))
+            print("Total P/L With Coin:", "${0:.2f}".format(total_p_l_coin))
+            print("━━━━━━━☆☆━━━━━━━…‥・")
             
-# print("Total P/L For Portfolio:", "${0:.2f}".format(total_p_l))
+print("Total P/L For Portfolio:", "${0:.2f}".format(total_p_l))
 
-# print("━━━━━━━☆☆━━━━━━━…‥・")
-# print("━━━━━━━☆☆━━━━━━━…‥・")
+print("━━━━━━━☆☆━━━━━━━…‥・")
+print("━━━━━━━☆☆━━━━━━━…‥・")
