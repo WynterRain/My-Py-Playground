8d1d52cdf88e978418c949f7e342e56683e8307a
SnowiWatermelon <heonaliu@gmail.com>
2021-10-06 15:20:22 -0400

    Add a test line

--- Project_GUI_Portfolio_App/Retry_GUI_Portfolio.py
+++ Project_GUI_Portfolio_App/Retry_GUI_Portfolio.py
@@ -6,6 +6,7 @@ pycrypto = Tk()
 pycrypto.title("My Crypto Portfolio")
 pycrypto.mainloop()
 
+print("Test Line")
 
 
 api_request = requests.get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=5&convert=USD&CMC_PRO_API_KEY=b2836bb2-a6a2-45e3-80a7-14a3810fe1e6")
