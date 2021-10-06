053937a25beb21f67c554d10e2e40bb43e2c3472
SnowiWatermelon <heonaliu@gmail.com>
2021-10-06 15:15:01 -0400

    Add Tk() class + Title + Mainloop

--- Project_GUI_Portfolio_App/Retry_GUI_Portfolio.py
+++ Project_GUI_Portfolio_App/Retry_GUI_Portfolio.py
@@ -2,6 +2,12 @@ from tkinter import *
 import requests 
 import json
 
+pycrypto = Tk()
+pycrypto.title("My Crypto Portfolio")
+pycrypto.mainloop()
+
+
+
 api_request = requests.get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=5&convert=USD&CMC_PRO_API_KEY=b2836bb2-a6a2-45e3-80a7-14a3810fe1e6")
 api = json.loads(api_request.content)
 
