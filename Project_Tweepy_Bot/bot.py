21fab8da70d48f681fb988ebcb3335456af4adb8
SnowiWatermelon <heonaliu@gmail.com>
2021-10-30 16:43:09 -0400

    add 2 functions ; for loop to iterate through the ids of messages

--- Project_Tweepy_Bot/bot.py
+++ Project_Tweepy_Bot/bot.py
@@ -7,8 +7,25 @@ secret = '0y52zkswuoJQEx4DnyH8iCkhs6iLLY99307Ut8KwzXgo1'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(key, secret)
-
 api = tweepy.API(auth)
-api.update_status('Hey From Twitter Bot - Second Tweet!')
 
-print("Status Updated!")
+FILE_NAME = 'Last_Seen.txt'
+
+def read_last_seen(FILE_NAME):
+    file_read = open(FILE_NAME, 'r')
+    last_seen_id = int(file_read.read().strip())
+    file_read.close()
+    return last_seen_id
+
+def store_last_seen(FILE_NAME, last_seen_id):
+    file_write = open(FILE_NAME, 'w')
+    file_write.write(str(last_seen_id))
+    file_write.close()
+    return
+
+tweets = api.mentions_timeline('1453783726362701840')
+for tweet in tweets:
+    print(str(tweet.id) + ' - ' + tweet.text)
+        
+    # if '#randomtweet' in tweet.text.lower():
+    #     print("New Tweet Found!")
