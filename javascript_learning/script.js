bd2ba6d48cb809bbd6b9f4757e9b1043197efb8d
SnowiWatermelon <heonaliu@gmail.com>
2021-12-31 10:47:15 -0500

    comment

--- javascript_learning/script.js
+++ javascript_learning/script.js
@@ -128,12 +128,12 @@
 
         
 
-function print(variable) {
-    return function func(variable2) {
-        console.log(variable)
-        console.log(variable2)
-    }
-}
+// function print(variable) {
+//     return function func(variable2) {
+//         console.log(variable)
+//         console.log(variable2)
+//     }
+// }
 
-let a = print(1)
-a(2)
+// let a = print(1)
+// a(2)
