453f3008b6ecf2d29dfb1a9df192968c1efa493b
SnowiWatermelon <heonaliu@gmail.com>
2021-12-31 10:23:03 -0500

    comment, the comments

--- javascript_learning/script.js
+++ javascript_learning/script.js
@@ -5,38 +5,38 @@
 // let a = true
 // let b = false
 
-// console.log(a && b ) // returns false (&& = and)
-// console.log(a || b ) // returns true (|| = or)
-// console.log(!a) // returns false (! = opposite of current value, you can put multiple '!' and it will invert however many times)
+// console.log(a && b ) // comment - returns false (&& = and)
+// console.log(a || b ) // comment - returns true (|| = or)
+// console.log(!a) // comment - returns false (! = opposite of current value, you can put multiple '!' and it will invert however many times)
 
-// // exercise 
+// //comment - exercise 
 
 // happy = true
 // fun = false
 
 // console.log(happy && fun)
 
-// // null & undefined
+// // comment -  null & undefined
 
-// undefined_var = undefined // no value => type = undefined
-// null_var = null // no value => type = object 
+// undefined_var = undefined // comment - no value => type = undefined
+// null_var = null // comment - no value => type = object 
 
-// // undefined : if you create a var [e.g : let xyz] with no val.. then type = undefined
-// // null : you specifically gave the variable no value by putting it's value to null
+// // comment - undefined : if you create a var [e.g : let xyz] with no val.. then type = undefined
+// //comment -  null : you specifically gave the variable no value by putting it's value to null
 
-// // compare variables
+// // comment - compare variables
 
 // let int_1 = 1
 // let int_2 = 2
 
-// console.log(int_1 == int_2) // double = signs for comparison-- returns true / false {LIKE PYTHON}
-// console.log(int_1 != int_2) // != signs for not equivelent to each other {LIKE PYTHON}
-// console.log(int_1 > int_2) // use greater or less than symbols for comparison {LIKE PYTHON}
-// console.log(int_1 <= int_2) // <= check less than or equal to/ greater than or equal to{LIKE PYTHON}
+// console.log(int_1 == int_2) // comment - double = signs for comparison-- returns true / false {LIKE PYTHON}
+// console.log(int_1 != int_2) // comment - != signs for not equivelent to each other {LIKE PYTHON}
+// console.log(int_1 > int_2) // comment - use greater or less than symbols for comparison {LIKE PYTHON}
+// console.log(int_1 <= int_2) // comment - <= check less than or equal to/ greater than or equal to{LIKE PYTHON}
 
-// console.log(undefined_var == null_var) // both have the same value with having the lack of a value so it's TRUE
+// console.log(undefined_var == null_var) // comment - both have the same value with having the lack of a value so it's TRUE
 
-// // functions
+// // comment - functions
 
 // function sayHello() {
 //     console.log("helloo")
@@ -44,7 +44,7 @@
 
 // sayHello()
 
-// // functions - exercise
+// //comment - functions - exercise
 
 // function my_name_exer() {
 //     console.log("Heona")
@@ -80,10 +80,10 @@
 //     return a+b
 // }
 
-// // let sumArrow = (a, b) => a+b   // << this is also equal to ^
+// // comment - let sumArrow = (a, b) => a+b   // << this is also equal to ^
 
 
-// // exercise - 1
+// //comment -  exercise - 1
 
 // let printName = (myName) => {
 //     console.log(myName)
@@ -91,13 +91,13 @@
 
 // printName("Heona")
 
-// // exercise -2 
+// // comment - exercise -2 
 
 // let printHi = name => "Hi " + name
 
 // console.log(printHi("mei"))
 
-// hoisting
+// comment -  hoisting
 
 
 function sum(a,b) {
@@ -107,5 +107,5 @@ function sum(a,b) {
  
 console.log(sum(1,2))
 
-// hoisting moves all your functions to the very top.. so if you put your function AFTER it is called; will still work
-// hoisting DOES NOT WORK with arrow functions (LET VARIABLES DO NOT GET HOISTED)
+// comment - hoisting moves all your functions to the very top.. so if you put your function AFTER it is called; will still work
+// comment - hoisting DOES NOT WORK with arrow functions (LET VARIABLES DO NOT GET HOISTED)
